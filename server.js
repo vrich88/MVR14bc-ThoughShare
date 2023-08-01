46074b2a7ddd6fb50f2be7ad1027d651e2efb228
vrich88 <mvrichards@icloud.com>
2023-08-01 12:37:15 -0500

    more typos

--- server.js
+++ server.js
@@ -5,15 +5,13 @@ const session = require("express-session");
 const hbs = require("express-handlebars");
 const helpers = require("./utils/helpers");
 const sequelize = require("./config/connection");
-const sessionStorage = require("connect-session-sequelize")(session.Store);
-
+const SessionStorage = require("connect-session-sequelize")(session.Store);
 // set up app & port
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 // session storage for 5 hours (adjust by 3600 per hour)
 const secretSession = {
-  secret: "sus",
+  secret: "shhh",
   cookie: {
     maxAge: 18000,
     httpOnly: true,
@@ -22,11 +20,10 @@ const secretSession = {
   },
   resave: false,
   saveUninitialized: true,
-  store: new sessionStorage({
+  store: new SessionStorage({
     db: sequelize,
   }),
 };
-
 // get the app using
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -36,7 +33,6 @@ app.use(require("./controllers/"));
 const view = hbs.create({ helpers });
 app.engine("handlebars", view.engine);
 app.set("view engine", "handlebars");
-
 // app running console log confirmation/location
 app.listen(PORT, () => {
   console.log(`Active at: ${PORT}`);
